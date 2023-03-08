476f970ecde9b7eb059196f0128d7c73dda6a5cb
ArtyomSinyugin <artyomsinyugin@gmail.com>
2023-03-08 12:41:56 +0300

    merge

---
+++ src/models/post_action.rs
@@ -0,0 +1,89 @@
+use crate::{
+    errors::AppError,
+    schema::{users, posts, comments}
+};
+use diesel::prelude::*;
+use serde::Serialize;
+
+use super::{user_action::User,comment_action::Comment};
+
+type Result<T> = std::result::Result<T, AppError>;
+
+#[derive(Debug, Queryable, Identifiable, Serialize, Associations)]
+#[belongs_to(User)]
+pub struct Post {
+    pub id: i32, 
+    pub user_id: i32,
+    pub title: String, 
+    pub body: String, 
+    pub published: bool,
+}
+
+pub fn create_post(conn: &mut SqliteConnection, user: &User, title: &str, body: &str) -> Result<Post> {
+    conn.transaction(|conn|{
+        diesel::insert_into(posts::table)
+            .values((
+                posts::user_id.eq(user.id),
+                posts::title.eq(title),
+                posts::body.eq(body),
+            ))
+            .execute(conn)?;
+
+        posts::table
+            .order(posts::id.desc())
+            .select(posts::all_columns)
+            .first(conn)
+            .map_err(Into::into)
+    })
+}
+
+pub fn publish_post(conn: &mut SqliteConnection, post_id: i32)-> Result<Post> {
+    conn.transaction (|conn| {
+        diesel::update(posts::table.filter(posts::id.eq(post_id)))
+            .set(posts::published.eq(true)) 
+            //.set((posts::published.eq(true), posts::title.eq("Mark".to_string()))) кортеж для апдейта нескольких полей
+            .execute(conn)?;
+
+        posts::table
+            .find(post_id)
+            .select(posts::all_columns)
+            .first(conn)
+            .map_err(Into::into)
+    })
+}
+
+pub fn all_posts(conn: &mut SqliteConnection) -> Result<Vec<((Post, User), Vec<(Comment, User)>)>> {   // было Result<Vec<(Post, User)>>
+    let query = posts::table
+        .order(posts::id.desc())
+        .filter(posts::published.eq(true))
+        .inner_join(users::table)
+        .select((posts::all_columns, (users::id, users::username)))
+         .load::<(Post, User)>(conn)?;
+    //let posts_with_user = query.load::<(Post, User)>(conn)?;
+    let (posts, post_users): (Vec<_>, Vec<_>) = query.into_iter().unzip();
+
+    let comments = Comment::belonging_to(&posts)
+        .inner_join(users::table)
+        .select((comments::all_columns, (users::id, users::username)))
+        .load::<(Comment, User)>(conn)?
+        .grouped_by(&posts);
+
+    Ok(posts.into_iter().zip(post_users).zip(comments).collect())
+}
+
+pub fn user_posts(conn: &mut SqliteConnection, user_id: i32)-> Result<Vec<(Post, Vec<(Comment, User)>)>> {
+    let posts = posts::table   
+        .filter(posts::user_id.eq(user_id))
+        .order(posts::id.desc())
+        .select(posts::all_columns)
+        .load::<Post>(conn)?;
+        //.map_err(Into::into)
+
+    let comments = Comment::belonging_to(&posts)
+        .inner_join(users::table)
+        .select((comments::all_columns, (users::id, users::username)))
+        .load::<(Comment, User)>(conn)?
+        .grouped_by(&posts);
+
+    Ok(posts.into_iter().zip(comments).collect())
+}
---
+++ src/models/user_action.rs
@@ -0,0 +1,49 @@
+use crate::{
+    errors::AppError,
+    schema::users
+};
+use diesel::prelude::*;
+use serde::Serialize;
+
+type Result<T> = std::result::Result<T, AppError>;
+
+#[derive(Debug, Queryable, Identifiable, Serialize, PartialEq)]
+pub struct User {
+    pub id: i32, 
+    pub username: String,
+}
+
+pub fn create_user(conn: &mut SqliteConnection, username: &str) -> Result<User> {
+    // стр. 116
+    conn.transaction(|conn| {
+        diesel::insert_into(users::table)
+            .values((users::username.eq(username),))
+            .execute(conn)?;
+ 
+        users::table   // я думаю, что смысл этого блока вернуть нам только что записанного пользователя из базы данных в нашу оперативную память (для дальнейшей работы с ним)
+            .order(users::id.desc())  // сортировка по убыванию. Зачем это?
+            .select((users::id, users::username))
+            .first(conn)           // сделал соединение мутабельным, потому что этого требовал этот метод!!!
+            .map_err(Into::into)
+    })
+}
+
+pub enum UserKey<'a> {
+    Username(&'a str),
+    ID(i32),
+}
+
+pub fn find_user<'a>(conn: &mut SqliteConnection, key: UserKey<'a>) -> Result<User> {
+    match key {
+        UserKey::Username(name) => users::table
+            .filter(users::username.eq(name))
+            .select((users::id, users::username))
+            .first::<User>(conn) // эквивалент load, только загружает первый элемент в списке, а не все отфильтрованные
+            .map_err(AppError::from),  // почему здесь не Into::into? интересно... 
+        UserKey::ID(id) => users::table
+            .filter(users::id.eq(id))
+            .select((users::id, users::username))
+            .first::<User>(conn)
+            .map_err(Into::into),      
+    }
+}
